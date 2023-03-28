991317199b0ad80f35c74cd63d2053e12ca3d251
uicklv <uicklv@gmail.com>
2023-03-28 20:29:18 +0300

    added new field

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <body>
 <form action="server.php" method="post" enctype="multipart/form-data">
     <input type="text" name="firstName">
+    <input type="text" name="lastName">
     <input type="file" name="file">
     <input type="submit">
 </form>
